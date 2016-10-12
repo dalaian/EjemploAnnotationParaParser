6798091920b91ea3a26d2a8121f2070c18c6d727
Dalaian <dalaian@outlook.com>
2016-10-12 08:49:26 -0600

    asd

---
+++ src/main/java/estructuras/ClasesVisibles.java
@@ -0,0 +1,13 @@
+package estructuras;
+
+public class ClasesVisibles
+{
+	private String[] nombresDeClasesVisibles;
+	
+	private String[] cargarNombresDeClasesVisibles()
+	{
+		//TODO Victor agarra las clases del biiuld path y las agrega al arreglo
+		return null;
+	}
+	
+}
